b1e6264a9ed8fbb32db286f1bf148c33dec6ac52
jghiasinejad <jghiasinejad@woolworths.com.au>
2023-08-30 00:09:19 +1000

    d

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -4,7 +4,7 @@ import { useSearchParams } from 'next/navigation'
 import { signIn } from 'next-auth/react'
 import GoogleSignInButton from '@/components/google-sign-in-button'
 
-export const SignIn = () =>  {
+const SignIn = () =>  {
     return<GoogleSignInButton />
 }
 
